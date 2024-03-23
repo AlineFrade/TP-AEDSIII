d4958bdda3638ffded4faff8d51040542169ad1a
Aline Frade <alinefrad@gmail.com>
2024-03-23 01:16:14 -0300

    addition of date type variable

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.Scanner;
 
 public class App {
@@ -20,6 +25,9 @@ public class App {
         Dado movie = new Dado();
         byte[] b;
         int id = 0;
+        SimpleDateFormat dateFormat = null;
+        Date data = null;
+        LocalDate localDate = null;
 
         String title = "", overview = "", input = "", genre = "";
         int popularity = 0, quantityGenre = 0;
@@ -41,31 +49,49 @@ public class App {
             System.out.println("u - update");
             System.out.println("d - delete");
             System.out.println("ou qualquer outra tecla para encerrar");
-            System.out.println("Escolha: ");
+            System.out.print("Escolha: ");
             Scanner scan = new Scanner(System.in);
             char entry;
             entry = scan.next().charAt(0);
             
             try {                
                 
+                System.out.println();
+
                 switch (entry) {
-            
+
                     // CREATE
                     case 'c':                    
                         // collecting the information about the data to be created 
                         System.out.print("Título: ");
                         title = reader.readLine();
+                        System.out.print("Digite a data no formato dd/MM/yyyy: ");
+                        String dataString = reader.readLine();
+
+                        // converting the data
+                        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                        try {
+                            data = dateFormat.parse(dataString);
+                        } catch (ParseException e) {
+                            System.out.println("Formato de data inválido. Use o formato dd/MM/yyyy");
+                            reader.close();
+                            return;
+                        }
+                        
+                        // Converta o objeto Date para LocalDate (apenas data, sem tempo)
+                        localDate = data.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
                         System.out.print("Overview: ");
                         overview = reader.readLine();
                         System.out.print("Popularidade: ");
                         input = reader.readLine();
                         popularity = Integer.parseInt(input);
-                        System.out.println("Quantidade de generos");
+                        System.out.print("Quantidade de generos: ");
                         input = reader.readLine();
                         quantityGenre = Integer.parseInt(input);
-                        System.out.println("Generos");
+                        System.out.print("Generos: ");
                         genre = reader.readLine();
-                        movie = new Dado(h.lastID+1, title, overview, popularity, quantityGenre, genre);
+                        movie = new Dado(h.lastID+1, title, localDate, overview, popularity, quantityGenre, genre);
 
                         b = movie.toByteArray();
                         path.create(b);
@@ -75,6 +101,7 @@ public class App {
                         
                         System.out.println("Dado criado com sucesso.");
 
+                        System.out.println();
                         break;
                 
                     // READ
@@ -87,6 +114,7 @@ public class App {
                         movie = path.read(movie, id);
                         System.out.println(movie);
                 
+                        System.out.println();
                         break;
 
                     // UPDATE
@@ -97,21 +125,39 @@ public class App {
                         int updateID = Integer.parseInt(stringID);
                         System.out.print("Título: ");
                         title = reader.readLine();
+
+                        System.out.print("Digite a data no formato dd/MM/yyyy: ");
+                        dataString = reader.readLine();
+
+                        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                        data = null;
+                        try {
+                            data = dateFormat.parse(dataString);
+                        } catch (ParseException e) {
+                            System.out.println("Formato de data inválido. Use o formato dd/MM/yyyy");
+                            reader.close();
+                            return;
+                        }
+                        
+                        // Converta o objeto Date para LocalDate (apenas data, sem tempo)
+                        localDate = data.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
                         System.out.print("Overview: ");
                         overview = reader.readLine();
                         System.out.print("Popularidade: ");
                         input = reader.readLine();
                         popularity = Integer.parseInt(input);
-                        System.out.println("Quantidade de generos");
+                        System.out.print("Quantidade de generos: ");
                         input = reader.readLine();
                         quantityGenre = Integer.parseInt(input);
-                        System.out.println("Generos");
+                        System.out.print("Generos: ");
                         genre = reader.readLine();
 
-                        movie = new Dado(updateID, title, overview, popularity, quantityGenre, genre);
+                        movie = new Dado(updateID, title, localDate, overview, popularity, quantityGenre, genre);
                         path.update(movie);
                         System.out.println("Dado atualizado com sucesso.");
 
+                        System.out.println();
                         break;
 
                     // DELETE
@@ -122,6 +168,7 @@ public class App {
                         path.delete(id);
 
                         System.out.println("Dado deletado com sucesso.");
+                        System.out.println();
                         break;
                         
                     // FINISH THE PROGRAM
@@ -131,6 +178,7 @@ public class App {
                         path.end();
                         System.out.println("Aplicação encerrada com sucesso.");
                     
+                        System.out.println();
                         break;
                 }
             } catch (Exception e) {
--- src/Dado.java
+++ src/Dado.java
@@ -3,23 +3,25 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
 
 // class used to create a data and manipulate the data
 public class Dado {
     protected String lapide, title, overview, genre;
     protected int id, popularity, quantityGenre;
-
+    protected LocalDate release;
 
     public Dado(){
 
     }
 
     // creation of a data with information given by the user
-    public Dado(int id, String title, String overview, int popularity,
+    public Dado(int id, String title, LocalDate release, String overview, int popularity,
                 int quantityGenre, String genre) throws IOException{
         lapide = "-";
         this.id = id;
         this.title = title;
+        this.release = release;
         this.overview = overview;
         this.popularity = popularity;
         this.quantityGenre = quantityGenre;
@@ -27,7 +29,7 @@ public class Dado {
     }
 
     public String toString() {
-        return "id: " + this.id + " | name: " + this.title + " | overview: " + this.overview + " | popularidade: " + popularity + " | quantidade de generos: " + quantityGenre + " | generos: " + genre;
+        return "id: " + this.id + " | name: " + this.title + " | release: " + release + " | overview: " + this.overview + " | popularidade: " + popularity + " | quantidade de generos: " + quantityGenre + " | generos: " + genre;
     }
 
     // convert the data to a array of bytes
@@ -37,6 +39,7 @@ public class Dado {
         dos.writeUTF(this.lapide);
         dos.writeInt(this.id);
         dos.writeUTF(this.title);
+        dos.writeLong(this.release.toEpochDay());
         dos.writeUTF(this.overview);
         dos.writeInt(this.popularity);
         dos.writeInt(this.quantityGenre);
@@ -50,6 +53,8 @@ public class Dado {
         DataInputStream dis = new DataInputStream(bais);
         this.id = dis.readInt();
         this.title = dis.readUTF();
+        long epochDay = dis.readLong();
+        this.release = LocalDate.ofEpochDay(epochDay);
         this.overview = dis.readUTF();
         this.popularity = dis.readInt();
         this.quantityGenre = dis.readInt();
